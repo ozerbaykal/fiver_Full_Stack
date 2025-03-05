4626c7d38d521e54eb2b7314a3bf285549f71140
ozerbaykal <baykalozer87@gmail.com>
2025-03-05 08:11:26 +0300

    complete category

--- client/src/pages/home/category.tsx
+++ client/src/pages/home/category.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { categories } from "../../utils/constants";
 
 const Category = () => {
-  return <div className="my-10"></div>;
+  return (
+    <div className="my-10 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-9 gap-5 ">
+      {categories.map((item, key) => (
+        <Link
+          to={`/search?category=${item.name}`}
+          key={key}
+          className="border shadow p-4 rounded-md cursor-pointer hover:shadow-xl hover:bg-gray-100"
+        >
+          <div className="flex flex-col gap-3 items-center">
+            <span className="text-3xl ">{item.icon}</span>
+            <span className="font-semibold text-sm">{item.name}</span>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
