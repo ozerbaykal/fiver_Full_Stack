b047875a5df9233ccd577204579e59dc4555853e
ozerbaykal <baykalozer87@gmail.com>
2025-03-05 07:14:36 +0300

    add:constants for category elemnts on utils and  add types.ts for introduce types

--- client/src/pages/home/hero.tsx
+++ client/src/pages/home/hero.tsx
@@ -1,13 +1,23 @@
+import { FormEvent } from "react";
 import { IoSearch } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const text = (e.currentTarget[0] as HTMLInputElement).value;
+
+    navigate(`/search?query=${text}`);
+  };
+
   return (
     <section className="bg-f-green max-md:m-[-20px] h-[40vh] px-6 py-6 md:px-12 md:py-10 md:rounded flex flex-col justify-center items-center text-white ">
       <div className="max-w-[600px]">
         <h1 className="text-4xl md:text-center md:text-5xl font-light">
           Profosyonel iş gücünüzü <span className="font-serif">freelancer'larla</span> ölçeklendirin
         </h1>
-        <form className="bg-white rounded-md w-full flex gap-5 mt-10">
+        <form onSubmit={handleSubmit} className="bg-white rounded-md w-full flex gap-5 mt-10">
           <input
             placeholder="hizmet ara"
             type="text"
